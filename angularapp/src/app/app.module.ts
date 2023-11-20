893484a311c9936b9d8f8e9b0da93fdb6018597f
Iamneo <techadmin@iamneo.ai>
2023-11-20 13:37:01 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ListteamsComponent } from './listteams/listteams.component';
 import { FindteamComponent } from './findteam/findteam.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ListteamsComponent,
-    FindteamComponent
+    FindteamComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
