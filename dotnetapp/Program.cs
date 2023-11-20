92da7f3cfe63ab19e29b2bb4a6f2c4e11f5a54a4
Iamneo <techadmin@iamneo.ai>
2023-11-20 12:22:08 +0000

    Backup Commit

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.Add
+builder.Services.AddDbContext<App
 
 var app = builder.Build();
 
